20eb5e50f6ff8790b7e95866f4eac8aa82727a95
Gustavo <gr4865423@gmail.com>
2026-03-20 20:04:36 -0300

    teste simples

--- app.py
+++ app.py
@@ -1,21 +1,7 @@
-print("BOT INICIANDO...")
-
-from flask import Flask, request, jsonify, render_template
-from bot import responder_mensagem
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
-
-@app.route('/chat', methods=['POST'])
-def chat():
-    data = request.get_json()
-    mensagem = data.get("mensagem")
-
-    resposta = responder_mensagem("teste", mensagem)
-
-    return jsonify({"resposta": resposta})
-
-# 🔥 IMPORTANTE: não depende do __main__ pro gunicorn
+    return "BOT ONLINE 🚀"
