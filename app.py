ed2393eeb9ea48fe4c6d9ddc29fa90df092ee2dd
Gustavo <gr4865423@gmail.com>
2026-03-20 19:59:02 -0300

    fix final railway

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ print("BOT INICIANDO...")
 
 from flask import Flask, request, jsonify, render_template
 from bot import responder_mensagem
-import os
 
 app = Flask(__name__)
 
-# Página principal (interface)
 @app.route('/')
 def home():
     return render_template("index.html")
 
-# Rota do chat
 @app.route('/chat', methods=['POST'])
 def chat():
     data = request.get_json()
@@ -21,8 +18,4 @@ def chat():
 
     return jsonify({"resposta": resposta})
 
-
-# 🚀 ESSA PARTE É A MAIS IMPORTANTE PRO RAILWAY
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+# 🔥 IMPORTANTE: não depende do __main__ pro gunicorn
